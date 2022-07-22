4771aef9b658d3b9020c6bdc2de080c5409ff136
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-07-22 02:40:07 -0500

    Configure main api using Graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`,`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `GraphQL`,
+        fieldName: `wpgraphql`,
+        url : `https://eventosyfestivales.com/graphql`
+      }
+    }
+  ],
 }
